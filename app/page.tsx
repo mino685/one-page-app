a4110b1da390e2e7798fbe0987268100ac4ca341
milan_kozak <milan.kozak94@gmail.com>
2025-03-11 18:15:30 +0100

    dsadsa

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-LALLAAL
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
@@ -14,7 +13,7 @@ LALLAAL
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Test.
           </li>
         </ol>
 
