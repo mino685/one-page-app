d20cf25ece231948ae097551e8eb8b631ded809d
milan_kozak <milan.kozak94@gmail.com>
2025-04-15 17:09:49 +0200

    change content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+ // or `v1X-appRouter` if you are using Next.js v1X
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <AppRouterCacheProvider>
         {children}
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
 }
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,59 @@
-import Image from "next/image";
+import { Avatar, Box, Card, CardActions, CardContent, CardHeader, CardMedia, CircularProgress, Collapse, Container, Grid, IconButton, Typography } from "@mui/material";
+
 
 export default function Home() {
   return (
-    <div style={{backgroundColor: '#C5AB9F'}} className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">   
-      <div style={{marginTop: '500px'}}><h1 style={{fontSize: '8rem'}}>NestFrame STUDIO</h1></div>
-      <code style={{fontSize: '4rem'}} className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-           In progress...
-            </code>
-      <h2 style={{fontSize: '2rem', color: 'grey'}}>B + M</h2>
-
-
-    </div>
+    <Container sx={{ bgcolor: '#fdf6e3' }}>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh' }}
+    >
+      <Card sx={{borderRadius: '5px'}}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: 'red' }} aria-label="recipe">
+              N
+            </Avatar>
+          }
+          title="NestFrame Studio"
+          subheader="July 15, 2025"
+        />
+        <CardContent>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            This impressive paella is a perfect party dish and a fun meal to cook
+            together with your guests. Add 1 cup of frozen peas along with the mussels,
+            if you like.
+          </Typography>
+        </CardContent>
+        <Collapse in={true} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography sx={{ marginBottom: 2 }}>Method:</Typography>
+            <Typography sx={{ marginBottom: 2 }}>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+              aside for 10 minutes.
+            </Typography>
+            <Typography sx={{ marginBottom: 2 }}>
+              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+              medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+              occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+              large plate and set aside, leaving chicken and chorizo in the pan. Add
+              pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+              stirring often until thickened and fragrant, about 10 minutes. Add
+              saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+            </Typography>
+            <Typography>
+              Barbi + Miňo
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+      </Grid>
+    
+  </Container>
   );
 }
+
